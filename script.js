f3255cbe72766a9854fd656c894c178054bc279b
Jakob <codemonkey727@gmail.com>
2024-08-29 19:01:53 -0400

    add styles to create 16x16 grid

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let numOfTiles = 0;
 const etchContainer = document.querySelector('.etch-container');
 
-//16 x 16 grid, means we need 136 tiles
-while(numOfTiles < 136){
+//16 x 16 grid, means we need 256 tiles
+while(numOfTiles < 256){
     let tile = document.createElement('div');
     tile.className = 'tile';
     etchContainer.appendChild(tile);
