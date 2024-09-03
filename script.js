685ea33af8b659783eaeff31c99fa9256980bfb7
Jakob <codemonkey727@gmail.com>
2024-09-03 18:16:53 -0400

    add reset button and change grid creation method
    so looking to the next part of the challenge i thought the way I
    was currently creating the div needed to be changed so i created a function
    to create the div. This allows it to be reusable and allow me to capture
    any input that is required.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let numOfTiles = 0;
-const etchContainer = document.querySelector('.etch-container');
+const body = document.querySelector('body');
 
 //16 x 16 grid, means we need 256 tiles
-while(numOfTiles < 256){
-    let tile = document.createElement('div');
-    tile.classList.add('tile');
-    tile.addEventListener('mouseover', () => {
-        tile.classList.add('filled-tile');
-    });
-    etchContainer.appendChild(tile);
-    numOfTiles++;
+function createGrid(rowLength = 16){
+    let totalTiles = rowLength ** 2;
+    let containerSize = 480;
+    let tileSize = containerSize/rowLength;
+
+    const etchContainer = document.createElement('div');
+    etchContainer.classList.add('etch-container');
+    body.appendChild(etchContainer);
+
+    while(numOfTiles < totalTiles){
+        const tile = document.createElement('div');
+
+        tile.style.width = tileSize + "px";
+        tile.style.height = tileSize + "px";
+
+        tile.addEventListener('mouseover', () => {
+            tile.classList.add('filled-tile');
+        });
+        etchContainer.appendChild(tile);
+
+        numOfTiles++;
+    }
 }
+
+createGrid();
