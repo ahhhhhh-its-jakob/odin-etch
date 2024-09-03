8e66d085fb62cb16015054f073ba744ab85f6a06
Jakob <codemonkey727@gmail.com>
2024-09-03 17:37:24 -0400

    add hover effect to our grids tiles
    here i changed the javascript to add an event listener to each tile
    when it is created. when the tile is moused over it adds a class
    that changes the background color4

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const etchContainer = document.querySelector('.etch-container');
 //16 x 16 grid, means we need 256 tiles
 while(numOfTiles < 256){
     let tile = document.createElement('div');
-    tile.className = 'tile';
+    tile.classList.add('tile');
+    tile.addEventListener('mouseover', () => {
+        tile.classList.add('filled-tile');
+    });
     etchContainer.appendChild(tile);
     numOfTiles++;
 }
